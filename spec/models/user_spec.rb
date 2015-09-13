87948a37acac0b523768f7b46bf331f03d8ac177
Jared_C <brasschaos@gmail.com>
2015-09-13 13:15:32 -0700

    cleans out file - comments out pending line

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,15 +1,5 @@
 require 'rails_helper'
-require 'spec_helper'
 
-
-
-  describe User do
-    it "is invalid without a name" do
-      FactoryGirl.build(:user, name: nil).should_not be_valid
-    end
-
-    it "has a valid factory" do
-      FactoryGirl.create(:user).should be_valid
-    end
-  end
-
+RSpec.describe User, type: :model do
+  #pending "add some examples to (or delete) #{__FILE__}"
+end
