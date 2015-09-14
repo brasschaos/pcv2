214b2ab3e985494e7505049e3b7f62350a66c302
Jared_C <brasschaos@gmail.com>
2015-09-13 17:37:10 -0700

    adds password_digest and true default to active row

--- db/migrate/20150908042834_create_users.rb
+++ db/migrate/20150908042834_create_users.rb
@@ -2,8 +2,8 @@ class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
       t.text :name
-      t.boolean :active
-
+      t.string :password_digest
+      t.boolean :active, :default => true
       t.timestamps
     end
   end
